dc7eb786e7dcdef1a2cd4b7737ade457350082d8
John Steven Dwyer <John@Johns-MacBook-Pro-2.local>
2015-10-21 17:16:57 -0700

    finished writing the code for the RPS game

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -5,7 +5,7 @@
 
 function getInput() {
     console.log("Please choose either 'rock', 'paper', or 'scissors'.")
-    return prompt();
+    return prompt("Please choose either 'rock', 'paper', or 'scissors'.");
 }
 function randomPlay() {
     var randomNumber = Math.random();
@@ -26,83 +26,94 @@ var playerMove;
 var computerMove;
 var playerWins;
 var computerWins;
-//var rock = "rock"; //Is defining rock, paper, scissocrs, necessary since the instructions tell us we're assuming them as the only values? 
-//var paper = "paper";
-//var scissors = "scissors";
+
 
 function getPlayerMove(move) {
-    return move || getInput();  /*Could this also be written as: return (move ? move : getInput()); ...I guess I would have to make var rock="rock"; var paper= "paper"; var scissor="scissor";, inorder to make rock, paper, scissors "truthey"(make it so it's not null) . Maybe?*/
-}   /*Write an expression that operates on a variable called `move`. If a `move` has a value, your expression should evaluate to that 
+    //console.log(move);
+    return move || getInput(); /*return move ? move : getInput() */    /*Could this also be written as: return (move ? move : getInput()); ...I guess I would have to make var rock="rock"; var paper= "paper"; var scissor="scissor";, inorder to make rock, paper, scissors "truthey"(make it so it's not null) . Maybe?*/
+    //playerMove = getPlayerMove;
+    //if(move){
+    //     return playerMove= move; 
+    // } else {
+    //     return playerMove = getInput();
+    // }
+}
+
+//getPlayerMove(rock, paper, scissors, shoot)
+/*Write an expression that operates on a variable called `move`. If a `move` has a value, your expression should evaluate to that 
     value. However, if `move` is not specified / is null, your expression should equal `getInput()`.*/
 
 
 function getComputerMove(move) {
-    return move || randomPlay();
+     //console.log(move);
+     return move || randomPlay(); /*return move ? move : randomPlay() */    
 }   /* Write an expression that operates on a variable called `move` If a `move` has a value, your expression should evaluate to that 
     value. However, if `move` is not specified / is null, your expression should equal `randomPlay()`.*/
 
 
 function getWinner(playerMove,computerMove) {
     var winner;
-        if(playerMove === computerMove) { 
+        if (playerMove === computerMove) { 
             winner = "tie";
-            consloe.log ("Tie!")
-    }
-        else if (playerMove === "rock") { /*//Can it be (if we keep rock, paper, scisscors undeclared variables): else if ((playerMove="rock"))*/
+            
+        } else if (playerMove === "rock") { /*//Can it be (if we keep rock, paper, scisscors undeclared variables): else if ((playerMove="rock"))*/
             if (computerMove === "scissors") {
-                winner= "Player";
-                //playerWins+=1; 
-        }   else {
-                winner= "Computer";
-                //computerWins+=1;
+                winner = "Player";
+                //playerWins++; 
+            } else {
+                winner = "Computer";
+                //computerWins++;
             }
-        }
-
-        else if (playerMove === "paper") {
+        } else if (playerMove === "paper") {
             if (computerMove === "rock") {
                 winner = "Player";
-                //layerWins+=1;
+                //playerWins++;
             } else {
                 winner = "Computer";
-                //computerWins+=1; 
+                //computerWins++; 
             }
-        }
-
-        else if (playerMove === "scissors") { 
+        } else if (playerMove === "scissors") { 
             if (computerMove === "paper") {
                 winner = "Player";
-                //playerWins+=1;
+                //playerWins++;
             } else {
                 winner = "Computer";
-                //computerWins+=1;
+                //computerWins++;
             }
         }     
 
     return winner;
 } 
 
+  /* Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
+    Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
+    The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.*/
 function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
+    console.log("Let's play Rock, Paper, Scissors")
     
     var playerWins = 0;/*Can I declare these values at the top where I declared most other things?*/ 
     var computerWins = 0; 
-    
-    var winner = getWinner(getPlayerMove(), getComputerMove());
-    console.log("Player chose " + playerMove + " while Computer chose " + computerMove);
 
-    getWinner(playerMove, computerMove);//Do I put playerMove and computerMove when I'm calling it since those are the inputs when the function was declared?
-    while ((playerWins <= 5) && (computerWins <= 5)){
-        if (winner === "player") {
-            playerWins ++;
+                      //KILL THIS getWinner(playerMove, computerMove);//Do I put playerMove and computerMove when I'm calling it since those are the inputs when the function was declared?
+    while (playerWins < 5 && computerWins < 5){
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        var roundWinner = getWinner(playerMove, computerMove);
+        console.log("Player chose " + playerMove + " while Computer chose " + computerMove);
+        
+        if (roundWinner === "Player") {
+            playerWins++;
             console.log("The score is " + playerWins + " to " + computerWins + "\n");
         }
-        else if (winner === "computer") { //Could it be "quicker" to replace this else...if with just an if--the else...if, in this case, doesn't need to run the if inorder for it 
-            computerWins ++;
+        else if (roundWinner === "Computer") { //Could it be "quicker" to replace this else...if with just an if--the else...if, in this case, doesn't need to run the if inorder for it 
+            computerWins++;
             console.log("The score is " + playerWins + " to " + computerWins + "\n");
         }
-        else if (winner === "tie"){
+        else if (roundWinner === "tie"){
+            console.log("Tie\n");
 
         }
+        console.log("playerWins:", playerWins, "\ncomputerWins:", computerWins);
     }
     if (playerWins === 5) {
             console.log("Player wins the game");
@@ -115,5 +126,3 @@ function playToFive() {
 }
 
 playToFive(playerMove,computerMove);
-
-
